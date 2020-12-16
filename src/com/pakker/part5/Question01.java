baea77795440aa7fb7da21820752826d98a40f0a
Pakker7 <43773175+Pakker7@users.noreply.github.com>
2020-12-17 00:13:45 +0900

    최소값, 최대값, 최대값과 인덱스

---
+++ src/com/pakker/part5/Question01.java
@@ -0,0 +1,33 @@
+package com.pakker.part5;
+
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Question01 {
+
+    //https://www.acmicpc.net/problem/10818
+    public static void main(String[] args) {
+        // good
+        Scanner scanner = new Scanner(System.in);
+        int nCnt = scanner.nextInt();
+        int[] nArray = new int[nCnt];
+        int i = 0;
+
+        while (i < nCnt) {
+            nArray[i++] = scanner.nextInt();
+        }
+
+        Arrays.sort(nArray);
+
+        System.out.println(nArray[0] + " " + nArray[nCnt-1]);
+
+        // 1차
+//        Scanner scanner = new Scanner(System.in);
+//        int nCnt = Integer.parseInt(scanner.nextLine());
+//        int[] nArray = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+//        System.out.print(Arrays.stream(nArray).min().getAsInt() + " " + Arrays.stream(nArray).max().getAsInt());
+
+    }
+}
---
+++ src/com/pakker/part5/Question02.java
@@ -0,0 +1,46 @@
+package com.pakker.part5;
+
+import java.util.Scanner;
+
+public class Question02 {
+
+    //https://www.acmicpc.net/problem/2562
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        int maxIndex = 0;
+        int max = 0;
+        while (i < 9) {
+            int n = scanner.nextInt();
+            if (n > max) {
+               max = n;
+               maxIndex = i;
+            }
+            i++;
+        }
+
+        System.out.println(max);
+        System.out.println(maxIndex + 1);
+
+        //1차
+//        Scanner scanner = new Scanner(System.in);
+//        int size = 9;
+//        int[] array = new int[size];
+//        int i = 0;
+//        while (i < size) {
+//            array[i++] = scanner.nextInt();
+//        }
+//
+//        int[] originArray = array.clone();
+//        Arrays.sort(array);
+//
+//        for (int a = 0; a < originArray.length; a++) {
+//            if (originArray[a] == array[size - 1]) {
+//                System.out.println(array[size - 1]);
+//                System.out.println(a + 1);
+//                break;
+//            }
+//        }
+    }
+}
