d67a4ad5ceb6cfe81167d2fb3756b66516cda1d6
THINKBIG\10014997 <psk@gmail.com>
2024-12-18 09:45:07 +0900

    이진탐색 ing..

--- src/com/pakker/dongbin/Question24.java
+++ src/com/pakker/dongbin/Question24.java
@@ -1,6 +1,8 @@
 package com.pakker.dongbin;
 
-public class Question23 {
+import java.util.Scanner;
+
+public class Question24 {
     //이진 탐색 소스코드 구현(반복문)
     public static int binarySearch(int[] arr, int target, int start, int end) {
         while (start <= end) {
@@ -21,6 +23,25 @@ public class Question23 {
     }
 
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        //원소의 개수(n)와 찾고자 하는 값(target)을 입력받기
+        int n = sc.nextInt();
+        int target=sc.nextInt();
+
+        //전체 원소 입력 받기
+        int[] arr = new int[n];
+        for(int i=0; i<n; i ++ ){
+            arr[i] = sc.nextInt();
+        }
+
+        //이진 탐색 수행결과 출력
+        int result = binarySearch(arr, target, 0, n-1);
+        if(result== -1) {
+            System.out.println("원소가 존재하지 않습니다.");
+        } else{
+            System.out.println(result +1);
+        }
 
     }
 }
